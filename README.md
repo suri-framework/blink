9ddf06d1b06875a45090a79edd4c4d38200a962d
Leandro Ostera <leandro@abstractmachines.dev>
2023-12-24 22:45:06 +0100

    docs: update readme

--- README.md
+++ README.md
@@ -14,49 +14,25 @@ First we create a connection, which we can reuse:
 
 <!-- $MDX file=./test/get_ocaml_org_test.ml,part=conn -->
 ```ocaml
-  let url = Uri.of_string "https://abstractmachines.dev" in
-  let conn =
-    match Blink.connect url with
-    | Ok conn -> conn
-    | Error err ->
-        (match err with
-        | `Closed -> Logger.error (fun f -> f "closed")
-        | `Invalid_uri uri -> Logger.error (fun f -> f "invalid_uri %a" Uri.pp uri)
-        | `Tls_error exn -> Logger.error (fun f -> f "tls_error %s" (Printexc.to_string exn))
-        | `Timeout -> Logger.error (fun f -> f "timeout")
-        | `Unix_error err -> Logger.error (fun f -> f "unix_error: %s" (Unix.error_message err)));
-        sleep 0.1;
-        Stdlib.exit 1
-  in
+let url = Uri.of_string "https://github.com" in
+let* conn = Blink.connect url in
 ```
 
 This figures out the protocol that we will use, and returns a `conn` value that we make requests with:
 
 <!-- $MDX file=./test/get_ocaml_org_test.ml,part=request -->
 ```ocaml
-  let req = Http.Request.make "/" in
-  let conn, _req_ref = Blink.request conn req () |> Result.get_ok in
+let req = Http.Request.make "/" in
+let* conn = Blink.request conn req () in
 ```
 
 Finally, once we have made a request, we can call `Blink.stream` to stream-parse the results and receive the parts as they come:
 
 <!-- $MDX file=./test/get_ocaml_org_test.ml,part=stream -->
 ```ocaml
-  let _conn, parts =
-    match Blink.stream conn with Ok x -> x | Error err -> 
-        (match err with
-        | `Closed -> Logger.error (fun f -> f "closed")
-        | `Response_parsing_error -> Logger.error (fun f -> f "response_parsing_error")
-        | `Invalid_uri uri -> Logger.error (fun f -> f "invalid_uri %a" Uri.pp uri)
-        | `Tls_error exn -> Logger.error (fun f -> f "tls_error %s" (Printexc.to_string exn))
-        | `Timeout -> Logger.error (fun f -> f "timeout")
-        | `Unix_error err -> Logger.error (fun f -> f "unix_error: %s" (Unix.error_message err)));
-        sleep 0.1;
-        Stdlib.exit 1
-
-  in
-
-  let[@warning "-8"] [ `Status status; `Headers headers; `Data body; `Done ] =  parts in
+let* _conn, [ `Status status; `Headers headers; `Data body; `Done ] =
+  Blink.stream conn
+in
 ```
 
 When you receive a `` `Done `` message you'd have reached the end of the stream.
--- test/get_ocaml_org_test.ml
+++ test/get_ocaml_org_test.ml
@@ -1,67 +1,38 @@
-open Riot
+[@@@warning "-8"]
 
-let () =
-  Riot.run @@ fun () ->
-  let _ = Logger.start () |> Result.get_ok in
-  Logger.set_log_level (Some Debug);
-  (* $MDX part-begin=conn *)
-  let url = Uri.of_string "https://github.com" in
-  let conn =
-    match Blink.connect url with
-    | Ok conn -> conn
-    | Error err ->
-        (match err with
-        | `Closed -> Logger.error (fun f -> f "closed")
-        | `Invalid_uri uri ->
-            Logger.error (fun f -> f "invalid_uri %a" Uri.pp uri)
-        | `Tls_error exn ->
-            Logger.error (fun f -> f "tls_error %s" (Printexc.to_string exn))
-        | `Timeout -> Logger.error (fun f -> f "timeout")
-        | `Unix_error err ->
-            Logger.error (fun f -> f "unix_error: %s" (Unix.error_message err)));
-        sleep 0.1;
-        Stdlib.exit 1
-  in
-  (* $MDX part-end *)
-  (* $MDX part-begin=request *)
-  let req = Http.Request.make "/" in
-  let conn = Blink.request conn req () |> Result.get_ok in
-  (* $MDX part-end *)
-  (* $MDX part-begin=stream *)
-  let _conn, parts =
-    match Blink.stream conn with
-    | Ok x -> x
-    | Error err ->
-        (match err with
-        | `Eof | `Closed -> Logger.error (fun f -> f "closed")
-        | `Response_parsing_error ->
-            Logger.error (fun f -> f "response_parsing_error")
-        | `Invalid_uri uri ->
-            Logger.error (fun f -> f "invalid_uri %a" Uri.pp uri)
-        | `Tls_error exn ->
-            Logger.error (fun f -> f "tls_error %s" (Printexc.to_string exn))
-        | `Timeout -> Logger.error (fun f -> f "timeout")
-        | `Unix_error err ->
-            Logger.error (fun f -> f "unix_error: %s" (Unix.error_message err)));
-        sleep 0.1;
-        Stdlib.exit 1
-  in
+let ( let* ) = Result.bind
 
-  let[@warning "-8"] [ `Status status; `Headers headers; `Data body; `Done ] =
-    parts
-  in
+open Riot;;
 
-  (* $MDX part-end *)
-  match (status, Http.Header.to_list headers, IO.Buffer.length body) with
-  | `OK, _, 0 ->
-      Logger.info (fun f -> f "get_ocaml_org_test: OK");
-      sleep 0.1;
-      shutdown ()
-  | _ ->
-      Logger.error (fun f ->
-          f "> Got response:\n%s\n%s\n%s\n%!"
-            (Http.Status.to_string status)
-            (Http.Header.to_lines headers |> String.concat "")
-            (IO.Buffer.to_string body));
-      sleep 0.1;
-      Stdlib.exit 1
+Riot.run @@ fun () ->
+Result.get_ok
+@@
+let _ = Logger.start () |> Result.get_ok in
+Logger.set_log_level (Some Debug);
+
+(* $MDX part-begin=conn *)
+let url = Uri.of_string "https://github.com" in
+let* conn = Blink.connect url in
+(* $MDX part-end *)
+(* $MDX part-begin=request *)
+let req = Http.Request.make "/" in
+let* conn = Blink.request conn req () in
+(* $MDX part-end *)
+(* $MDX part-begin=stream *)
+let* _conn, [ `Status status; `Headers headers; `Data body; `Done ] =
+  Blink.stream conn
+in
+(* $MDX part-end *)
+match (status, Http.Header.to_list headers, IO.Buffer.length body) with
+| `OK, _, 0 ->
+    Logger.info (fun f -> f "get_ocaml_org_test: OK");
+    sleep 0.1;
+    Ok (shutdown ())
+| _ ->
+    Logger.error (fun f ->
+        f "> Got response:\n%s\n%s\n%s\n%!"
+          (Http.Status.to_string status)
+          (Http.Header.to_lines headers |> String.concat "")
+          (IO.Buffer.to_string body));
+    sleep 0.1;
+    Stdlib.exit 1
