87f27bed88fa012dfd1f23987a554bbf88d84451
Leandro Ostera <leandro@abstractmachines.dev>
2023-12-26 21:36:28 +0100

    fix: inline licenses from ocaml-tls and ca-certs

--- blink/ssl.ml
+++ blink/ssl.ml
@@ -1,16 +1,67 @@
-(**
-  The majority of this module has been taken verbatim (and in some places refactored to work with Riot)
-  from `ocaml-tls` and its `eio` subpackage, namely:
-  * https://github.com/mirleft/ocaml-tls/blob/main/eio/tls_eio.ml
-  * https://github.com/mirleft/ocaml-tls/blob/main/eio/x509_eio.ml
+(******************************************************************************************
 
-  And from `ca-certs`:
-  * https://github.com/mirage/ca-certs/blob/main/lib/ca_certs.ml
+  The Tls_unix below was ported from  `ocaml-tls`, its `eio` subpackage, specifically from:
+    * https://github.com/mirleft/ocaml-tls/blob/main/eio/tls_eio.ml
+    * https://github.com/mirleft/ocaml-tls/blob/main/eio/x509_eio.ml
 
-  See their licenses:
-  * https://github.com/mirleft/ocaml-tls/blob/main/LICENSE.md
-  * https://github.com/mirage/ca-certs/blob/main/LICENSE.md
-*)
+  under this license:
+
+    Copyright (c) 2014, David Kaloper and Hannes Mehnert
+    All rights reserved.
+    
+    Redistribution and use in source and binary forms, with or without modification,
+    are permitted provided that the following conditions are met:
+    
+    * Redistributions of source code must retain the above copyright notice, this
+      list of conditions and the following disclaimer.
+    
+    * Redistributions in binary form must reproduce the above copyright notice, this
+      list of conditions and the following disclaimer in the documentation and/or
+      other materials provided with the distribution.
+    
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+    ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+    ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+******************************************************************************************
+
+  The X509 and Auth modules below were ported from `ca-certs`, specifically from:
+    * https://github.com/mirage/ca-certs/blob/main/lib/ca_certs.ml
+  
+  under this license:
+  
+    Copyright (c) 2014, David Kaloper and Hannes Mehnert
+    All rights reserved.
+    
+    Redistribution and use in source and binary forms, with or without modification,
+    are permitted provided that the following conditions are met:
+    
+    * Redistributions of source code must retain the above copyright notice, this
+      list of conditions and the following disclaimer.
+    
+    * Redistributions in binary form must reproduce the above copyright notice, this
+      list of conditions and the following disclaimer in the documentation and/or
+      other materials provided with the distribution.
+    
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+    ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+    ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*******************************************************************************************)
 
 open Riot
 
